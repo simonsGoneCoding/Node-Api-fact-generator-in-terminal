ea26f1a10ae6797c13f282dc04cde3d2b06feaf6
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-12 14:53:57 +0000

    Add fetch returning a fact from given year.

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 // http://numbersapi.com/random/year?json
 
 const fetch = require("node-fetch");
+//npm package enabling coloring terminal messeges
+const colors = require("colors");
+colors.enable();
 
 //get year from terminal
 const year = process.argv[2];
 
 //fetch from numbers.api
-fetch(`http://numbersapi.com/2000/year?json`).then(
-  (res) => {
-    if (res.ok) {
-      return res.json();
+fetch(`http://numbersapi.com/${year}/year?json`)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
     }
-    throw new Error("Request failed!");
-  },
-  (networkError) => console.log(networkError.message)
-);
+  })
+  .then((jsonRes) => console.log(jsonRes.text.italic.yellow))
+  .catch((err) => console.log("Errrrrrorrr!!!".red, err));
